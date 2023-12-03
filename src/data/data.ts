d636b6b4279c18598724419b8d5ce0751a5053ab
Jayme Hoffman <jaymeh13@gmail.com>
2023-12-03 21:26:51 +0100

    Adding Dawn Wallet

--- src/data/data.ts
+++ src/data/data.ts
@@ -140,6 +140,98 @@ export const wallets: Record<string, Features & Info> = {
       availableTestnets: false,
     },
   },
+  'Dawn Wallet': {
+    url: 'https://www.dawnwallet.xyz/',
+    deviceCompatibility: {
+      mobile: true,
+      desktop: false,
+      browser: true,
+    },
+    accountType: {
+      eoa: false,
+      erc4337: false,
+      safe: true,
+    },
+    chainCompatibility: {
+      ethereum: true,
+      optimism: true,
+      arbitrum: true,
+      base: true,
+      polygon: true,
+      gnosis: false,
+      bnbSmartChain: false,
+    },
+    ensCompatibility: {
+      mainnet: true,
+      subDomains: true,
+      offchain: false,
+      L2s: true,
+      customDomains: false,
+      freeUsernames: false,
+    },
+    backupOptions: {
+      cloud: false,
+      local: true,
+      socialRecovery: true,
+    },
+    securityFeatures: {
+      multisig: true,
+      MPC: false,
+      keyRotation: true,
+      transactionScanning: true,
+      limitsAndTimelocks: true,
+      hardwareWalletSupport: true,
+    },
+    availableTestnets: {
+      availableTestnets: true,
+    },
+  },
+  Rainbow: {
+    url: 'https://rainbow.me/',
+    deviceCompatibility: {
+      mobile: true,
+      desktop: false,
+      browser: true,
+    },
+    accountType: {
+      eoa: true,
+      erc4337: false,
+      safe: false,
+    },
+    chainCompatibility: {
+      ethereum: true,
+      optimism: true,
+      arbitrum: true,
+      base: true,
+      polygon: true,
+      gnosis: false,
+      bnbSmartChain: true,
+    },
+    ensCompatibility: {
+      mainnet: true,
+      subDomains: true,
+      offchain: true,
+      L2s: true,
+      customDomains: false,
+      freeUsernames: false,
+    },
+    backupOptions: {
+      cloud: true,
+      local: true,
+      socialRecovery: false,
+    },
+    securityFeatures: {
+      multisig: false,
+      MPC: false,
+      keyRotation: false,
+      transactionScanning: true,
+      limitsAndTimelocks: false,
+      hardwareWalletSupport: true,
+    },
+    availableTestnets: {
+      availableTestnets: false,
+    },
+  },
   'Safe Wallet': {
     url: 'https://app.safe.global/',
     deviceCompatibility: {
