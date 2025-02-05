81b1be4220756b746b09e92d0a0f0a0e6e4b85d3
polymutex <polymutex@protonmail.com>
2025-02-05 16:05:27 +0800

    Static site export configuration.

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  distDir: 'out',
   reactStrictMode: true,
   modularizeImports: {
     '@mui/icons-material': {
