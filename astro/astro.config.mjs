d09c0e2a44937873ee86c13b15f63e0e340d4e99
Darryl Yeo <dev@darryl-yeo.com>
2025-02-05 22:48:45 -0600

    Vite: handle CommonJS code from MUI

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,9 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import react from '@astrojs/react';
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
-});
+	integrations: [
+		react(),
+	],
+	vite: {
+		ssr: {
+			noExternal: [
+				'@mui/*',
+			],
+			external: [
+				'@mui/x-internals',
+			],
+		},
+	},
+})
